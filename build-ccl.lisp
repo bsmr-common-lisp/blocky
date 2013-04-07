32d35355d86393e5117f04492f6c3b2d3048e634
dto <dto@ioforms.org>
2013-04-07 17:13:05 -0400

    build43

--- build-ccl.lisp
+++ build-ccl.lisp
@@ -1,3 +1,5 @@
+(push (merge-pathnames "lib/" *default-pathname-defaults*)
+      asdf:*central-registry*)
 (ql:quickload '(:lispbuilder-sdl-mixer :lispbuilder-sdl-ttf :lispbuilder-sdl-image :uuid :cl-opengl :cl-fad))
 (asdf:load-system :2x0ng)
 (ccl:save-application "2x0ng.exe" 
