fe6cc12bf78bc0183940e704cfd50ccb9e72275d
dto <dto@ioforms.org>
2013-07-06 21:38:50 -0400

    app type

--- build-ccl.lisp
+++ build-ccl.lisp
@@ -7,6 +7,7 @@
 ;; (ql:quickload '(:lispbuilder-sdl-mixer :lispbuilder-sdl-ttf :lispbuilder-sdl-image :uuid :cl-opengl :cl-fad))
 (asdf:load-system :2x0ng)
 (ccl:save-application "2x0ng.exe" 
+		      :application-type :gui
 		      :prepend-kernel t
 		      :toplevel-function '2x0ng:2x0ng)
 
